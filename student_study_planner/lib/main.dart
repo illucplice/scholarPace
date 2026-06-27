9f3087ead1b9d0efc09bf7c393f1b8efcc9190ff
illucplice <gubhajupranish12@gmail.com>
2026-06-27 22:02:19 +0545

    feat: add login screen UI with logo and container

--- student_study_planner/lib/main.dart
+++ student_study_planner/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screen/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,11 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
     );
   }
 }
